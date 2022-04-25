f6c76064ea6b178943b303c68216585631be5418
VoHongHoa <19521522@gm.uit.edu.vn>
2022-04-25 09:19:21 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Nav } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
+import MoviesList from "./Component/movies-list";
+import AddReview from "./Component/add-review";
+import Movies from "./Component/movie";
+import Login from "./Component/login";
 function App() {
+  const [user, setUser] = React.useState(null);
+  async function login(user = null) {
+    setUser(user);
+  }
+  async function logout() {
+    setUser(null);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Võ Hồng Hòa 19521522</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>
+              <Link to={"/movies"}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link>
+              {user ? (
+                <a onClick={logout}>Logout User</a>
+              ) : (
+                <Link to={"/login"}>Login</Link>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Switch>
+        <Route exact path={["/", "/movies"]} component={MoviesList}></Route>
+        <Route
+          path="/movies/:id/review"
+          render={(props) => <AddReview {...props} user={user} />}
+        ></Route>
+        <Route
+          path="/movies/:id/"
+          render={(props) => <Movies {...props} user={user} />}
+        ></Route>
+        <Route
+          path="/login"
+          render={(props) => <Login {...props} login={login} />}
+        ></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/Component/add-review.js
@@ -0,0 +1,5 @@
+import React from "react";
+function AddReview() {
+  return <div className="App">AddReview</div>;
+}
+export default AddReview;
